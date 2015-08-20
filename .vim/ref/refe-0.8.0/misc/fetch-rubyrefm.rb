b4756b0b964988d27ab6d75695b789485df6d24c
alpaca-tc <alpaca-tc@alpaca.tc>
2015-08-20 14:44:49 +0900

    Remove references

--- .vim/ref/refe-0.8.0/misc/fetch-rubyrefm.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-require 'net/http'
-Net::HTTP.get_print 'www.ruby-lang.org', '/ja/man/man-rd-ja.tar.gz'
--- .vim/ref/ruby-ref1.9.2/refe-1_8_7.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd "%~dp0"
-ruby -Ke -I bitclust/lib bitclust/bin/refe.rb -d db-1_8_7 -e sjis %*
-popd
