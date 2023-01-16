975950d9fc2c0b763f23b7a2fbb193b34581f95d
alpaca-tc <alpaca-tc@alpaca.tc>
2023-01-16 13:32:06 +0900

    add path

--- .vim/lua/rails.lua
+++ .vim/lua/rails.lua
@@ -7,12 +7,43 @@ function M.setup()
     group = group,
     pattern = "*",
     callback = function()
-      local cwd = vim.fn["expand"]('%:p')
+      local cwd = vim.fn["expand"]("%:p")
 
       if vim.fn["alpaca#is_rails"](cwd) == 1 then
         vim.api.nvim_exec_autocmds("User", { pattern = "Rails" })
       end
-    end
+    end,
+  })
+
+  -- Add Rails path
+  vim.api.nvim_create_autocmd("User", {
+    group = group,
+    pattern = "Rails",
+    callback = function()
+      local path = {
+        "app/controllers",
+        "app/controllers/concerns",
+        "app/models",
+        "app/models/concerns",
+        "app/mailers",
+        "app/mailers/concerns",
+        "app/repositories",
+        "app/validators",
+        "app/view_models",
+        "app/workers",
+        "app/decorators",
+        "app/jobs",
+        "app/forms",
+        "app/policies",
+        "lib",
+      }
+
+      local root = vim.fn["alpaca#current_root"](vim.fn["getcwd"]())
+
+      for _, val in pairs(path) do
+        vim.opt_local.path:prepend(root .. "/" .. val)
+      end
+    end,
   })
 end
 
