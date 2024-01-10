e72fefe2f0d8a97a5b60d94668690c2429ffc8a5
alpaca-tc <alpaca-tc@alpaca.tc>
2024-01-10 22:48:42 +0900

    update python version

--- .vim/init.lua
+++ .vim/init.lua
@@ -11,7 +11,7 @@ vim.o.termguicolors = true
 vim.o.clipboard=vim.o.clipboard..','..'unnamedplus'
 
 if fn.executable('pyenv') then
-  vim.g.python3_host_prog = os.getenv('HOME') .. '/.pyenv/versions/3.11.1/bin/python'
+  vim.g.python3_host_prog = os.getenv('HOME') .. '/.pyenv/versions/3.12.1/bin/python'
   vim.g.python_host_prog = os.getenv('HOME') .. '/.pyenv/versions/2.7.18/bin/python'
 else
   vim.g.python_host_prog = 'python'
@@ -19,7 +19,7 @@ end
 
 if fn.executable('rbenv') then
   -- vim.g.ruby_host_prog = fn.system({ 'rbenv', 'which', 'ruby' })
-  vim.g.ruby_host_prog = os.getenv('HOME') .. '/.rbenv/versions/3.1.2/bin/ruby'
+  vim.g.ruby_host_prog = os.getenv('HOME') .. '/.rbenv/versions/3.2.2/bin/ruby'
 else
   vim.g.ruby_host_prog = 'ruby'
 end
